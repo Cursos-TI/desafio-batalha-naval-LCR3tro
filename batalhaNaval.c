5b778b7abe07ec1769b6e6e157632daf1ca9c2c1
Lucas Silva Cerqueira <127765724+LCR3tro@users.noreply.github.com>
2025-11-05 01:47:34 +0000

    Versão intermédiaria

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+#include <windows.h>
 
 #define TAM 10
 
 int main(){
- 
+SetConsoleOutputCP(65001);
+ char letras[10]={'A','B','C','D','E','F','G','H','I','J'};
 int tabuleiro[TAM][TAM];
 int i, j;
 
@@ -14,22 +15,31 @@ for( j = 0;j < TAM; j++){
 }
 }
 
-//i = coluna , j = linha e todos serão impressos na 3 linha
+//i = linha vertical , j = linha horizontal e todos serão impressos na 3 linha
 
-//Primeiro Navio
+//Primeiro Navio vertical
 for(j = 0; j < 4;j++){
-    tabuleiro[2][2] = 1;
+    tabuleiro[3][3] = 3;
 }
-//segundo navio
+//segundo navio horizontal
 for(i= 0; i<3; i++){
-    tabuleiro[2][3] = 1;
+    tabuleiro[2][3] = 3;
 }
-//Terceiro navio
+//Terceiro navio Diagonal
 for(i= 0;i<5;i++){
-    tabuleiro[2][4] = 1;
+    tabuleiro[1][4] = 3;
+}
+//Quarto navio diagonal
+for(i=0;i< 5;i++){
+    tabuleiro[1][2] = 3;
 }
 
 printf("====Tabuleiro batalha Naval====\n");
+
+for(j=0;j<10;j++){
+printf("%c",letras[j]);
+}
+printf("\n");
 
 for(i= 0;i < TAM;i++){
     for(j= 0;j < TAM;j++){
