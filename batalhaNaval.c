09e7daa2116016a1a98cdc42634b2146b6fd8ae3
Lucas Silva Cerqueira <127765724+LCR3tro@users.noreply.github.com>
2025-11-15 02:58:30 +0000

    Versão FInal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,54 +1,69 @@
 #include <stdio.h>
-#include <windows.h>
 
-#define TAM 10
+
+//aplicar habilidade
+void habilidade(char tabuleiro[10][10], int *habilidade, int linhas, int colunas, int linhas_meio, int colunas_meio, char marca){
+ int inicio_linhas = linhas_meio - (linhas / 2);
+    int inicio_colunas = colunas_meio - (colunas / 2);
+
+for(int i = 0; i < linhas; i++){
+    for(int j = 0; j< colunas;j++){
+        if(*(habilidade + i*colunas + j) == 1){
+            if (inicio_linhas + i >= 0 && (inicio_linhas + i) < 10 &&
+                inicio_linhas + j >= 0 && inicio_colunas + j < 10) {
+                    tabuleiro[inicio_linhas + i][inicio_colunas + j] = marca;
+                }
+            }
+        }
+    }
+}
+
 
 int main(){
-SetConsoleOutputCP(65001);
- char letras[10]={'A','B','C','D','E','F','G','H','I','J'};
-int tabuleiro[TAM][TAM];
+char tabuleiro[10][10];
 int i, j;
 
-for( i = 0; i < TAM; i++){
-for( j = 0;j < TAM; j++){
-    tabuleiro[i][j] = 0;
+for( i = 0; i < 10; i++){
+for( j = 0;j < 10; j++){
+    tabuleiro[i][j] = '0';
 }
 }
 
-//i = linha vertical , j = linha horizontal e todos serão impressos na 3 linha
+// Define as habilidades
+    int cone[3][5] = {
+        {0,0,1,0,0},
+        {0,1,1,1,0},
+        {1,1,1,1,1}
+    };
 
-//Primeiro Navio vertical
-for(j = 0; j < 4;j++){
-    tabuleiro[3][3] = 3;
-}
-//segundo navio horizontal
-for(i= 0; i<3; i++){
-    tabuleiro[2][3] = 3;
-}
-//Terceiro navio Diagonal
-for(i= 0;i<5;i++){
-    tabuleiro[1][4] = 3;
-}
-//Quarto navio diagonal
-for(i=0;i< 5;i++){
-    tabuleiro[1][2] = 3;
-}
+    int cruz[5][5] = {
+        {0,0,1,0,0},
+        {0,0,1,0,0},
+        {1,1,1,1,1},
+        {0,0,1,0,0},
+        {0,0,1,0,0}
+    };
+
+    int octaedro[3][3] = {
+        {0,1,0},
+        {1,1,1},
+        {0,1,0}
+    };
+
+    habilidade(tabuleiro, (int*)cone, 3,5,8,2,'1');
+    habilidade(tabuleiro, (int*)cruz, 5,5,6,6,'5');
+    habilidade(tabuleiro, (int*)cone, 3,5,3,2,'4');
+
 
 printf("====Tabuleiro batalha Naval====\n");
-
-for(j=0;j<10;j++){
-printf("%c",letras[j]);
-}
-printf("\n");
-
-for(i= 0;i < TAM;i++){
-    for(j= 0;j < TAM;j++){
-        printf("%d", tabuleiro[i][j]);
+ printf("   A B C D E F G H I J\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d  ", i);
+for (int j = 0; j < 10; j++) {
+            printf("%c ", tabuleiro[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");
-}
-
-
 return 0;
 
 }
